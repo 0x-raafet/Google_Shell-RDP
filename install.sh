392178a32c6f6e7ba5eea610b201938f546d54b8
cobra shell <109506437+0x-raafet@users.noreply.github.com>
2023-06-29 20:42:20 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 echo "Preparing...."
-echo -e "Installing ngrok..."
 wget -O ngrok-stable-linux-amd64.zip "https://github.com/0x-raafet/Google_Shell-RDP/blob/main/ngrok-stable-linux-amd64.zip?raw=true" > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1; rm ngrok-stable-linux-amd64.zip 2> /dev/null
 sudo mv ./ngrok /bin/ngrok; chmod 777 /bin/ngrok
